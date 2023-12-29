a99fe271ee968487aafb065769466c2081e8a931
Shubham-0310 <kunwarahlawat@gmail.com>
2023-12-29 13:09:29 +0530

    meitView

--- js/main.js
+++ js/main.js
@@ -19,39 +19,22 @@ $( document ).ready(function(){
             }
         }
     })
+
+
+    //  *************************************** collapse show more and show less 
     
-    $('#example').vSelect({
-    
-        // allows multiselect
-        multiSelect: true,
-      
-        // custom placeholder
-        placeholder: 'Please select',
-      
-        // enable Check All checkbox
-        checkAll: true,
-        checkAllLabel: 'All',
-      
-        // expand all option groups on load
-        expanded: true,
-      
-        // or "values"
-        display: 'sum',
-      
-        // height of the dropdown
-        trayHeight: '240px',
-      
-        // false = make the dropdown inline block
-        dropdown: true,
-      
-        // allows to search options
-        search: false,
-        onChange:function(values, options) {
-       
-              console.log(values)
-     
-            }
-          
+    let collapseText = $('.collapseText');
+    let showMoreBtn = $('.collapseExample');
+
+    showMoreBtn.click(function () {
+      if (showMoreBtn.hasClass('collapsed')) {
+        showMoreBtn.html("Show More &nbsp; <i class='bi bi-chevron-down'></i>");
         
-      });
+      } else {
+        showMoreBtn.html("Show Less &nbsp; <i class='bi bi-chevron-up'></i>");
+    
+      }
+    });
+        //  *************************************** collapse show more and show less  end
+
 })
